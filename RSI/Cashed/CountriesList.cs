8c6254935013c5c2035577557a3914b89e5ef60a
mazbros <mazbros@gmail.com>
2016-08-03 17:20:15 -0400

    Added classes and models for NONUS

---
+++ RSI/Cashed/CountriesList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RSI.Cashed
+{
+    public class CountriesList
+    {
+        private static readonly Lazy<CountriesList> Inst = new Lazy<CountriesList>();
+
+        private List<string> _countries;
+        public static CountriesList Instance => Inst.Value;
+
+        public List<string> Get()
+        {
+            if (_countries != null) return _countries;
+
+            var doctors = DoctorsList_NONUS.Instance.Get();
+            _countries = doctors.OrderBy(c => c.Country).Select(c => c.Country).Distinct().ToList();
+            return _countries;
+        }
+    }
+}
---
+++ RSI/Cashed/SpecialtiesList_NONUS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RSI.Cashed
+{
+    public class SpecialtiesList_NONUS
+    {
+        private static readonly Lazy<SpecialtiesList_NONUS> Inst = new Lazy<SpecialtiesList_NONUS>();
+
+        private List<string> _specialities;
+        public static SpecialtiesList_NONUS Instance => Inst.Value;
+
+        public List<string> Get()
+        {
+            if (_specialities != null) return _specialities;
+
+            var doctors = DoctorsList_NONUS.Instance.Get();
+            _specialities = doctors.OrderBy(s => s.Specialty).Select(s => s.Specialty).Distinct().ToList();
+            return _specialities;
+        }
+    }
+}
